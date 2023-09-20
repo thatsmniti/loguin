eb32c43343d774d2ac8ea7e60990d5cf8895781c
thatsmniti <134503971+thatsmniti@users.noreply.github.com>
2023-09-20 07:57:06 -0300

    Add files via upload

---
+++ loguin/login.js
@@ -0,0 +1,22 @@
+lgn = "julia";
+pwd = "12356";
+
+function autenticar(){
+var log = document.getElementById('login').value;
+var pass = document.getElementById('passwd').value;
+
+if(log== lgn & pass == pwd){
+   alert('fon')
+}else{
+alert ('Firifonfon')
+
+
+
+}
+
+
+
+}
+
+
+
